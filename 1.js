ce1c7aae36b545aacfb8bbb35ca5ad6fa7cea752
Mahardhika Pratama <putrad578@gmail.com>
2023-07-12 09:30:22 +0700

    revision 1.js untuk keterangan dan code

--- 1.js
+++ 1.js
@@ -1,21 +1,21 @@
 //Penggunaan try catch
 
-const cekHariKerja = (day) => {
-    setTimeout(() => {
-      try {
-        const dataDay = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat'];
-        let cek = dataDay.find((whichDay) => whichDay.toLowerCase() === day.toLowerCase());//find untuk mengembalikan nilai pertama yang sesuai kondisi
-        if (cek) {
-          console.log(`Sekarang adalah hari ${cek}, maka kerja`);
-        } else {
-          throw new Error('Bukan hari kerja');
-        }
-      } catch(error) {
-        console.log(error);
-      }
-    }, 1000);
-};
-cekHariKerja('Rabu')
+// const cekHariKerja = (day) => {
+//     setTimeout(() => {
+//       try {
+//         const dataDay = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat'];
+//         let cek = dataDay.find((whichDay) => whichDay.toLowerCase() === day.toLowerCase());//find untuk mengembalikan nilai pertama yang sesuai kondisi
+//         if (cek) {
+//           console.log(`Sekarang adalah hari ${cek}, maka kerja`);
+//         } else {
+//           throw new Error('Bukan hari kerja');
+//         }
+//       } catch(error) {
+//         console.log(error);
+//       }
+//     }, 1000);
+// };
+// cekHariKerja('Rabu')
 
 // try dan catch digunakan dalam pemrograman JavaScript untuk menangani kesalahan yang terjadi dalam bagian kode yang bersifat synchronous (sinkron). Memungkinkan penanganan kesalahan dalam blok kode yang dapat menimbulkan exception (pengecualian).
 
@@ -29,7 +29,9 @@ cekHariKerja('Rabu')
   //       const dataDay = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat'];
   //       let cek = dataDay.find((whichDay) => whichDay.toLowerCase() === day.toLowerCase());//find untuk mengembalikan nilai pertama yang sesuai kondisi
   //       if (cek) {
-  //         resolve(`Sekarang adalah hari ${cek}, maka kerja`);
+  //         setTimeout(()=>{
+  //           resolve(`Sekarang adalah hari ${cek}, maka kerja`);
+  //         }, 4000)
   //       } else {
   //         reject(new Error('Bukan hari kerja'));
   //       }
@@ -40,7 +42,7 @@ cekHariKerja('Rabu')
   // cekHariKerja('Senin').then((result) => {
   //   console.log(result)
   // }).catch((err) => {
-  //   console.log(err)
+  //   console.log(err.message)
   // });
 
-  // then dan catch digunakan dalam pemrograman berbasis Promise untuk menangani hasil yang berhasil (resolve) dan penolakan (reject) dari operasi asinkron. Mereka memungkinkan penanganan yang terstruktur terhadap hasil dan kesalahan yang terjadi.
+  // then dan catch digunakan dalam pemrograman berbasis Promise untuk menangani hasil yang berhasil (resolve) dan penolakan (reject) dari operasi asinkron. Memungkinkan penanganan yang terstruktur terhadap hasil dan kesalahan yang terjadi.
